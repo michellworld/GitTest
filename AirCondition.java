e638dbe33474711f15643131815e456d456dde64
Michael Dada <michell4dollars.com>
2019-05-14 11:42:47 -0400

    AirCondition.java

---
+++ AirCondition.java
@@ -0,0 +1,27 @@
+public class AirCondition {
+	
+	private String brand;
+	private String color;
+	private int weight;
+	private int size;
+	
+	
+	public void setColor(String color){
+		this.color = color;
+	}
+
+	public String getColor(){
+		return color;
+}
+
+	public void setWeight(int weight){
+		this.weight = weight;
+	}
+
+	public int getWeight(){
+		return weight;
+	}
+
+}
+	
+	
